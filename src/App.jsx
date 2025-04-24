5de4b294c3f731e19f7cf4c7750afcb58cdf9b78
Ahmad Mario <riotheassassin@gmail.com>
2025-04-24 20:59:14 +0700

    REF: BrowserRouter at main entry app

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import LoginPage from "./components/pages/LoginPage";
 import LandingPage from "./components/pages/LandingPage";
-import DashboardPage from "./components/pages/DashboardPage"
+import DashboardPage from "./components/pages/DashboardPage";
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<LoginPage />} />
-        <Route path="/home" element={<LandingPage />} />
-        <Route path="/dashboard" element={<DashboardPage />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<LoginPage />} />
+      <Route path="/home" element={<LandingPage />} />
+      <Route path="/dashboard" element={<DashboardPage />} />
+    </Routes>
   );
 };
 
