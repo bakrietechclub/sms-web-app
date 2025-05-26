4f903e6522dfa6bde2b23742d3fbef25d388cf10
Ahmad Mario <riotheassassin@gmail.com>
2025-05-27 05:10:34 +0700

    FEAT: Detail on Audience & CoorGroup Page

--- src/components/pages/univ/CoordinationGroup.jsx
+++ src/components/pages/univ/CoordinationGroup.jsx
@@ -3,9 +3,16 @@ import { Table } from "../../fragments/Table";
 import { useState } from "react";
 import { TableToolbar } from "../../fragments/TableToolbar";
 import { Pagination } from "../../fragments/Pagination";
+import WAIcon from "../../../assets/icons/whatsappIcon.png";
+import { ChevronLeft } from "lucide-react";
 
 export const CoordinationGroup = () => {
+  const [showDetail, setShowDetail] = useState(false);
   const [search, setSearch] = useState("");
+
+  const handleClick = () => {
+    setShowDetail(!showDetail);
+  };
 
   const whatsappLink = "https://wa.me/";
   const data = [
@@ -81,6 +88,14 @@ export const CoordinationGroup = () => {
     },
   ];
 
+  const [selected, setSelected] = useState({
+    name: "",
+    division: "",
+    jenis: "",
+    link: "",
+    contact: "",
+  });
+
   const headers = [
     "No",
     "Nama Instansi",
@@ -102,43 +117,138 @@ export const CoordinationGroup = () => {
           className="text-white bg-[#E89229] rounded-lg w-full py-1.5 hover:py-1 hover:bg-[#d18325] ease-in-out duration-200 cursor-pointer"
           onClick={() => window.open(value.link)}
         >
-          <div className="underline">Link Grup</div>
+          <div className="flex underline items-center justify-center gap-2">
+            <img src={WAIcon} alt="." />
+            Link Grup
+          </div>
         </Button>
       </td>
       <td>{value.contact}</td>
       <td>
-        <a href="#" className="text-[#0D4690] underline">
+        <Button
+          className="text-[#0D4690] underline cursor-pointer"
+          onClick={() => {
+            handleClick();
+            setSelected(value);
+          }}
+        >
           Lihat Detail
-        </a>
+        </Button>
       </td>
     </tr>
   );
 
-  return (
-    <div>
-      <h1 className="text-2xl font-semibold">Tabel Grup Koordinasi</h1>
-      <TableToolbar
-        searchValue={search}
-        onSearchChange={setSearch}
-        onAddClick={(type) => {
-          if (type === "Kategori A") openModalA();
-          if (type === "Kategori B") openModalB();
-        }}
-        addOptions={["Kategori A", "Kategori B"]}
-        filters={[
-          {
-            label: "Jenis Instansi",
-            options: [
-              { label: "Universitas", value: "universitas" },
-              { label: "Lembaga Sosial", value: "lembaga sosial" },
-            ],
-          },
-        ]}
-        onFilterSet={() => console.log("Filter diset")}
-        searchWidth="w-1/4"
-      />
-      <Table headers={headers} data={data} renderRow={renderRow} />
-      <Pagination />
-    </div>
-  );
+  if (!showDetail) {
+    return (
+      <div>
+        <h1 className="text-2xl font-semibold">Tabel Grup Koordinasi</h1>
+        <TableToolbar
+          searchValue={search}
+          onSearchChange={setSearch}
+          onAddClick={(type) => {
+            if (type === "Kategori A") openModalA();
+            if (type === "Kategori B") openModalB();
+          }}
+          addOptions={["Kategori A", "Kategori B"]}
+          filters={[
+            {
+              label: "Jenis Instansi",
+              options: [
+                { label: "Universitas", value: "universitas" },
+                { label: "Lembaga Sosial", value: "lembaga sosial" },
+              ],
+            },
+          ]}
+          onFilterSet={() => console.log("Filter diset")}
+          searchWidth="w-1/4"
+        />
+        <Table headers={headers} data={data} renderRow={renderRow} />
+        <Pagination />
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <Button
+          className={"text-[#0D4690] cursor-pointer flex"}
+          onClick={() => {
+            handleClick();
+          }}
+        >
+          <ChevronLeft /> Kembali
+        </Button>
+        <h1 className="text-2xl font-semibold mt-4">
+          Data Lengkap Grup Koordinasi
+        </h1>
+        <div className="flex justify-end">
+          <Button
+            className={
+              "bg-[#0D4690] text-white cursor-pointer rounded-md px-4 py-2"
+            }
+          >
+            Perbarui
+          </Button>
+        </div>
+        <div className="grid grid-cols-2 gap-y-7 mb-7">
+          <div className="">
+            <p className="font-semibold">Nama Instansi:</p>
+            <p className="ml-2">{selected.name}</p>
+          </div>
+          <div className="">
+            <p className="font-semibold">Jenis Instansi</p>
+            <p className="ml-2">{selected.jenis}</p>
+          </div>
+          <div className="">
+            <p className="font-semibold">Divisi Instansi:</p>
+            <p className="ml-2">{selected.division}</p>
+          </div>
+        </div>
+        <div className="grid grid-cols-1 gap-y-7 mb-7">
+          <div className="">
+            <p className="font-semibold">Link Grup:</p>
+            <a
+              href={selected.link}
+              className="ml-2 text-[#0D4690] italic underline"
+            >
+              Clickable Link
+            </a>
+          </div>
+        </div>
+        <div className="grid grid-cols-2 gap-y-7 mb-7">
+          <div className="">
+            <p className="font-semibold">Kontak</p>
+          </div>
+          <div className="">
+            <p className="font-semibold">Tambah Kontak</p>
+          </div>
+        </div>
+        <table className="table-auto text-center w-full">
+          <thead className="text-[#0D4690] bg-[#E7EDF4]">
+            <tr className="h-10 text-base font-semibold">
+              <th className="rounded-tl-xl">No.</th>
+              <th className="">Nama</th>
+              <th className="">Jabatan</th>
+              <th className="">No. Hp</th>
+              <th className="">Email</th>
+              <th className="">Status</th>
+              <th className="">Status Aktif</th>
+              <th className="rounded-tr-xl">Aksi</th>
+            </tr>
+          </thead>
+          <tbody className="text-base border-l border-r border-[#E7EDF4]">
+            <tr className="border-b border-[#E7EDF4] h-10 font-normal">
+              <td className="py-3">No.</td>
+              <td className="">Nama</td>
+              <td className="">Jabatan</td>
+              <td className="">No. Hp</td>
+              <td className="">Email</td>
+              <td className="">Status</td>
+              <td className="">Status Aktif</td>
+              <td className="">Aksi</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    );
+  }
 };
